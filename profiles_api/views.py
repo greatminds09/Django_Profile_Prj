0b96753769ca9f22ca24c336e9c1ee041537fc7c
Jayamanikandan Hemachandran <greatminds09@gmail.com>
2020-06-15 00:52:32 -0400

    API View changes and Serializers

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,16 @@
 #from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+"""This status package has a list of handy status codes that can be used when handing HTTP Status"""
 
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
+    """ This stmt above will initialize this API and tells what to expect when a POST HTTP request is called"""
+    """ In this case it will know that it needs to expect a Name tag with a max char of 10"""
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -16,3 +22,30 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message':'Hello!', 'an_apiview':an_apiview})
+
+    def post(self, request):
+        """ Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            email = serializer.validated_data.get('email')
+            message = f'Hello {name} your email is {email}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+                )
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method':'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle partial update of an object"""
+        return Response({'method':'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an Object"""
+        return Response({'message':'DELETE'})
